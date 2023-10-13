d296fb52e1aff27174e5cb1c5f66b2a15a1798b1
Inkwan Hwang <ikhwang@postech.ac.kr>
2023-10-13 20:52:42 +0900

    FEAT process_execute

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -38,8 +38,16 @@ process_execute (const char *file_name)
     return TID_ERROR;
   strlcpy (fn_copy, file_name, PGSIZE);
 
+  /************* Project 2-2 Argument Passing *************/
+  char* save_ptr;
+  char* thread_name = strtok_r(fn_copy, " ", &save_ptr);
+  /********************************************************/
+  
   /* Create a new thread to execute FILE_NAME. */
-  tid = thread_create (file_name, PRI_DEFAULT, start_process, fn_copy);
+  /************* Project 2-2 Argument Passing *************/
+  //tid = thread_create (file_name, PRI_DEFAULT, start_process, fn_copy);
+  tid = thread_create (thread_name, PRI_DEFAULT, start_process, fn_copy);
+  /********************************************************/
   if (tid == TID_ERROR)
     palloc_free_page (fn_copy); 
   return tid;
