7d9b7a8f7db17ac5a843ea5ba4b1099ad32b74bb
athen-a <jjijji121@gmail.com>
2023-10-31 23:48:44 +0900

    Feat: fage fault

--- src/userprog/exception.c
+++ src/userprog/exception.c
@@ -5,6 +5,7 @@
 #include "threads/interrupt.h"
 #include "threads/thread.h"
 
+#include "userprog/process.h"
 /* Number of page faults processed. */
 static long long page_fault_cnt;
 
@@ -148,6 +149,18 @@ page_fault (struct intr_frame *f)
   write = (f->error_code & PF_W) != 0;
   user = (f->error_code & PF_U) != 0;
 
+   if (!user)
+   {
+      f->eip = f->eax;
+      f->eax = -1;
+      return;
+   }
+   if (user)
+   {
+      printf("%s: exit(%d)\n", thread_name(), -1);
+      thread_current()->pcb->exit_code = -1;
+      thread_exit();
+   }
   /* To implement virtual memory, delete the rest of the function
      body, and replace it with code that brings in the page to
      which fault_addr refers. */
